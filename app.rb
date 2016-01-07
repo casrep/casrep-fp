4c6373c712b2dfd3ed3eee4a2709789e6020898f
casrep <derekcjansen@gmail.com>
2016-01-06 19:24:40 -0600

    Updating broken app.

--- app.rb
+++ app.rb
@@ -6,7 +6,7 @@ require 'thin'
 # begin
 #   $db = Mysql.new('localhost', 'root', 'TaykMeeOn-55', 'local')
 # rescue Mysql::Error
-#   puts "Connection fall down, go boom."
+#   puts 'Connection fall down, go boom.'
 #   exit 1
 # end
 
@@ -16,25 +16,29 @@ get '/mywot*.html' do
 end
 
 get '/' do
-	@title = "Home (CASREP)"
+	@title = 'Home (CASREP)'
+	@page = 'Home'
   erb :home
 end
 
 get '/interesting' do
-	@title = "Interesting (CASREP)"
+	@title = 'Interesting (CASREP)'
+	@page = 'Things of Interest'
 	erb :interesting
 end
 
 get '/projects' do
-	@title = "Projects (CASREP)"
+	@title = 'Projects (CASREP)'
+	@page = 'Projects'
 	erb :projects
 end
 
 get '/about' do
-	@title = "About (CASREP)"
+	@title = 'About (CASREP)'
+	@page = 'About'
 	erb :about
 end
 
 # get '/posts' do
-# 	@results = $db.query("SELECT * FROM posts")
+# 	@results = $db.query('SELECT * FROM posts')
 # end
